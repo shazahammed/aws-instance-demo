6185001ec7fd2daf39ff79458b29a17567db3d81
Shaz Ahammed <77955202+shazahammed@users.noreply.github.com>
2023-09-22 11:27:37 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/receive-iot-data", receiveIoTData)
+
+	port := 8443 
+	addr := fmt.Sprintf(":%d", port)
+	fmt.Printf("Listening on port %d...\n", port)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		fmt.Println("Error starting HTTP server:", err)
+	}
+}
+
+func receiveIoTData(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body", http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Println("Received IoT data:", string(body))
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Data received successfully"))
+	} else {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
+}
